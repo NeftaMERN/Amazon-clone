b29f04eeecaf208acafed93114c65752321273e9
Neftalem <dneftalem@gmail.com>
2025-04-30 15:41:45 +0300

    add comment & space

--- Function/Sell JS/app.js
+++ Function/Sell JS/app.js
@@ -1,4 +1,5 @@
 
+// scroll buttons
 const scrollContainer = document.querySelector('.Main_Movies_Image');
 const scrollLeft = document.getElementById('scroll-left');
 const scrollRight = document.getElementById('scroll-right');
